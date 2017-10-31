f64a2afcbce83a0096db7b2189552ed110e3eca7
Germán Küber <german.kuber@outlook.com>
2017-10-31 19:46:30 -0300

    Update readme.md

---
+++ src/Models/TpViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace src.Models
+{
+    public class TpViewModel
+    {
+        public string Group { get; set; }
+        public DateTime Date { get; set; }
+        public int MembersQuantity { get; set; }
+
+    }
+}

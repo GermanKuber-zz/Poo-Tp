b814a857f06f30611de735c532ce83467e2ca194
Germán Küber <german.kuber@outlook.com>
2017-10-31 21:04:28 -0300

    Added models

--- src/Models/TpViewModel.cs
+++ src/Models/TpViewModel.cs
@@ -4,9 +4,27 @@ namespace src.Models
 {
     public class TpViewModel
     {
+        #region  Properties
+
         public string Group { get; set; }
         public DateTime Date { get; set; }
+        
         public int MembersQuantity { get; set; }
+
+        #endregion
+
+        #region 
+
+        public TpViewModel(string group,
+                           DateTime date,
+                           int membersQuantity)
+        {
+            this.Group = group;
+            this.Date = date;
+            this.MembersQuantity = membersQuantity;
+        }
+
+        #endregion
 
     }
 }
